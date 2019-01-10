a5f4b6ab68d1e6f47278df369ba297d697ed9ecd
Uk <ukchukundah@gmail.com>
2019-01-10 11:22:05 +0100

    Add models

---
+++ app/Models/Student.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'sch_student_bio';
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,30 +1,14 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class User extends Authenticatable
+class User extends Model implements Authenticatable
 {
-    use Notifiable;
+    protected $table = 'sup_users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function getAuthPassword() {
+        $this->hash_code;
+    }
 }
