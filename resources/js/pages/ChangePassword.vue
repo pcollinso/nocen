2710f740835123221588b7fc6bcb00263898708e
paulcollins <pcollinsmb@gmail.com>
2019-01-22 11:42:39 +0100

    CHANGE PASSWORD VIEW

---
+++ resources/js/pages/ChangePassword.vue
@@ -0,0 +1,264 @@
+<template>
+  <Page>
+    <!-- begin login-cover -->
+    <div class="login-cover">
+      <div class="login-cover-image" :style="{ backgroundImage: 'url('+ bg.activeImg +')' }"></div>
+      <div class="login-cover-bg"></div>
+    </div>
+    <!-- end login-cover -->
+    <!-- begin login -->
+    <div class="login login-v2" data-pageload-addclass="animated fadeIn">
+      <div class="login-header">
+        <div class="brand">
+          <img src="/images/logo.png" height="60">
+        </div>
+        <div class="icon">
+          <i class="fa fa-lock"></i>
+        </div>
+      </div>
+      <!-- begin login-content -->
+      <div class="login-content">
+        <form
+          ref="form"
+          action="/change-password"
+          @submit.prevent="checkForm()"
+          method="POST"
+          class="margin-bottom-0"
+        >
+          <input type="hidden" name="_token" v-model="form.token">
+
+          <div  class="form-group m-b-20">
+            <label>Change Password</label>
+          </div>
+          <div class="form-group m-b-20">
+            <input
+              type="password"
+              name="user_password"
+              class="form-control form-control-lg"
+              placeholder="Current Password"
+              v-model.trim="form.password"
+              required
+            >
+          </div>
+            <div class="form-group m-b-20">
+                <input
+                    type="password"
+                    name="new_password"
+                    class="form-control form-control-lg"
+                    placeholder="New Password"
+                    v-model.trim="form.password"
+                    required
+                >
+            </div>
+
+            <div class="form-group m-b-20">
+                <input
+                    type="password"
+                    name="confirm_password"
+                    class="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    v-model.trim="form.password"
+                    required
+                >
+            </div>
+
+          <div class="login-buttons">
+            <button type="submit" class="btn btn-primary btn-block btn-lg">{{ buttonText }}</button>
+            <a href="/dashboard" class="btn btn-danger btn-block btn-lg">Back To Dashboard</a>
+          </div>
+          <!--<div class="m-t-20">
+						Not a member yet? Click <a href="javascript:;">here</a> to register.
+          </div>-->
+        </form>
+      </div>
+      <!-- end login-content -->
+    </div>
+    <!-- end login -->
+    <!-- begin login-bg -->
+    <ul class="login-bg-list clearfix">
+      <li v-bind:class="{ 'active': bg.bg1.active }">
+        <a
+          href="javascript:;"
+          v-on:click="select('bg1')"
+          style="background-image: url(/images/login-bg/login-bg-16.jpg)"
+        ></a>
+      </li>
+      <li v-bind:class="{ 'active': bg.bg2.active }">
+        <a
+          href="javascript:;"
+          v-on:click="select('bg2')"
+          style="background-image: url(/images/login-bg/login-bg-17.jpg)"
+        ></a>
+      </li>
+      <li v-bind:class="{ 'active': bg.bg3.active }">
+        <a
+          href="javascript:;"
+          v-on:click="select('bg3')"
+          style="background-image: url(/images/login-bg/login-bg-15.jpg)"
+        ></a>
+      </li>
+      <li v-bind:class="{ 'active': bg.bg4.active }">
+        <a
+          href="javascript:;"
+          v-on:click="select('bg4')"
+          style="background-image: url(/images/login-bg/login-bg-14.jpg)"
+        ></a>
+      </li>
+      <li v-bind:class="{ 'active': bg.bg5.active }">
+        <a
+          href="javascript:;"
+          v-on:click="select('bg5')"
+          style="background-image: url(/images/login-bg/login-bg-13.jpg)"
+        ></a>
+      </li>
+      <li v-bind:class="{ 'active': bg.bg6.active }">
+        <a
+          href="javascript:;"
+          v-on:click="select('bg6')"
+          style="background-image: url(/images/login-bg/login-bg-12.jpg)"
+        ></a>
+      </li>
+    </ul>
+    <!-- end login-bg -->
+  </Page>
+</template>
+
+<script>
+import Page from './Page';
+import FilterMixin from '../mixins/FilterMixin';
+import PageOptions from '../components/PageOptions';
+
+export default {
+	name: 'ChangePassword',
+  mixins: [FilterMixin],
+	components: {
+		Page
+  },
+	created() {
+		PageOptions.pageEmpty = true;
+	},
+	beforeRouteLeave (to, from, next) {
+		PageOptions.pageEmpty = false;
+		next();
+	},
+	data() {
+		return {
+      step: 1,
+      name: 'Test',
+      form: {
+        new_password: '',
+        user_password: '',
+        confirm_password: '',
+        type: '',
+        token: ''
+      },
+			bg: {
+        activeImg: '/images/login-bg/login-bg-16.jpg',
+        bg1: {
+            active: true,
+            img: '/images/login-bg/login-bg-16.jpg'
+        },
+        bg2: {
+            active: false,
+            img: '/images/login-bg/login-bg-17.jpg'
+        },
+				bg3: {
+					active: false,
+					img: '/assets/img/login-bg/login-bg-15.jpg'
+				},
+				bg4: {
+					active: false,
+					img: '/images/login-bg/login-bg-14.jpg'
+				},
+				bg5: {
+					active: false,
+					img: '/images/login-bg/login-bg-13.jpg'
+				},
+				bg6: {
+					active: false,
+					img: '/images/login-bg/login-bg-12.jpg'
+				}
+			}
+		}
+  },
+  computed: {
+    buttonText() {
+      return this.step === 1 ? 'Submit' : 'Submit';
+    },
+    firstStep() {
+      return this.step === 1;
+    },
+    secondStep() {
+      return this.step === 2;
+    }
+  },
+  mounted() {
+    this.form.token = document.querySelector('meta[name="csrf-token"]').content;
+  },
+	methods: {
+		checkForm() {
+      if (this.secondStep) {
+        this.$refs.form.submit();
+        return true;
+      }
+
+      axios
+      .post('/auth/change-password', this.form)
+      .then(({ data: { success, data } }) => {
+        if (!success) {
+          alert('Could not find user');
+          return;
+        }
+
+        if (data.verification_no) {
+          this.form.type = 'staff';
+        } else if (data.regno) {
+          this.form.type = 'student';
+        } else {
+          this.form.type = 'user';
+        }
+        this.name = data.full_name;
+
+        this.step = 2;
+      })
+      .catch(({ response: { data } }) => console.log("error", data));
+      return false;
+		},
+		select(x) {
+			this.bg.bg1.active = false;
+			this.bg.bg2.active = false;
+			this.bg.bg3.active = false;
+			this.bg.bg4.active = false;
+			this.bg.bg5.active = false;
+			this.bg.bg6.active = false;
+
+			switch (x) {
+				case 'bg1':
+					this.bg.bg1.active = true;
+					this.bg.activeImg = this.bg.bg1.img;
+					break;
+				case 'bg2':
+					this.bg.bg2.active = true;
+					this.bg.activeImg = this.bg.bg2.img;
+					break;
+				case 'bg3':
+					this.bg.bg3.active = true;
+					this.bg.activeImg = this.bg.bg3.img;
+					break;
+				case 'bg4':
+					this.bg.bg4.active = true;
+					this.bg.activeImg = this.bg.bg4.img;
+					break;
+				case 'bg5':
+					this.bg.bg5.active = true;
+					this.bg.activeImg = this.bg.bg5.img;
+					break;
+				case 'bg6':
+					this.bg.bg6.active = true;
+					this.bg.activeImg = this.bg.bg6.img;
+					break;
+			}
+		}
+	}
+}
+</script>
---
+++ resources/views/auth/change-password.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="/images/favicon.ico">
+
+        <title>Laravel</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div id="app">
+              <Change-Password />
+
+            </div>
+        </div>
+        <script type="text/javascript" src="{{ asset('js/manifest.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
