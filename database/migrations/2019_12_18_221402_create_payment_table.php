71e18a1f5709baf84f7db85a28100f0d197d27b3
paulcollins <pcollinsmb@gmail.com>
2019-06-10 09:33:06 +0100

    created fee and payment tables

--- database/migrations/2019_12_18_221402_create_payment_table.php
+++ database/migrations/2019_12_18_221402_create_payment_table.php
@@ -18,12 +18,13 @@ class CreatePaymentTable extends Migration
             $table->bigInteger('fee_id')->unsigned()->index();
             $table->tinyInteger('payment_type_id')->unsigned()->index()->comment('this can be 0 for full, 1 for first payment and 2 for second payment');
             $table->bigInteger('institution_id')->unsigned()->index();
-            $table->bigInteger('level_id')->unsigned()->index();
+            $table->bigInteger('level_id')->nullable();
             $table->string('j_regno', 100)->nullable()->index();
             $table->string('regno', 100)->nullable()->index();
             $table->unsignedDecimal('amount', 10, 2);
             $table->string('confirmation_no', 100)->nullable()->index();
             $table->string('receipt_no', 100)->nullable()->index();
+            $table->string('terminal_id', 100)->nullable()->index();
             $table->timestamps();
 
             $table->unique(['institution_id','fee_id','j_regno','regno'], 'DUPLICATE_REG_NO');
@@ -32,7 +33,6 @@ class CreatePaymentTable extends Migration
             $table->foreign('institution_id')->references('id')->on('sup_institution')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('fee_id')->references('id')->on('sch_fee')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('payment_type_id')->references('id')->on('sys_payment_type')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('level_id')->references('id')->on('sch_level')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
