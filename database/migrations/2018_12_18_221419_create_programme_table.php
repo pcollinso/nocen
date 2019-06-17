eca5a9284651eecebab7b4e2888563b5496c2181
paulcollins <pcollinsmb@gmail.com>
2019-06-17 11:16:10 +0100

    modified sch_programme table to contain fiels for controlling fee checks

--- database/migrations/2018_12_18_221419_create_programme_table.php
+++ database/migrations/2018_12_18_221419_create_programme_table.php
@@ -17,9 +17,12 @@ class CreateProgrammeTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('institution_id')->unsigned()->index();
             $table->string('programme_name', 200);
+            $table->boolean('require_jamb')->default('1');
+            $table->boolean('require_result_check_fee')->default('1');
+            $table->boolean('require_application_fee')->default('1');
+            $table->boolean('require_acceptance_fee')->default('1');
+            $table->boolean('require_school_fee')->default('1');
             $table->bigInteger('entered_by')->nullable();
-            $table->bigInteger('last_modified_by')->nullable();
-            $table->integer('status')->default('1');
             $table->timestamps();
             $table->unique(['institution_id','programme_name'], 'DUPLICATE_RECORD');
             $table->foreign('institution_id')->references('id')->on('sup_institution')->onDelete('cascade')->onUpdate('cascade');
