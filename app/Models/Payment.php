fe7962c0682d0d36550dd004786e018e35635f9e
paulcollins <pcollinsmb@gmail.com>
2019-07-11 15:25:07 +0100

    payment model class made to inherit trait for insertignore and insert on duplicate key update trait

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Models;
+use App\Traits\InsertOnDuplicateKey;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+  use InsertOnDuplicateKey;
   protected $table = 'sch_payment';
 }
